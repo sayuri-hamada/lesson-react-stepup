453641a86659bf1b9fe614f67fbd986d09eba4c7
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-09 16:56:36 +0900

    模擬的にレンダリングコストの高いコンポーネントを作成

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -8,6 +8,14 @@ const style = {
 export const ChildArea = (props) => {
   //親からopenの値を受け取って三項演算子で表示を制御
   const { open } = props;
+  console.log("ChildAreaがレンダリングされた");
+
+  //コンポーネントのレンダリング負荷が上がる処理を入れる
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (
